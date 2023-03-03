ef86f51cc9b5ddac7d5048da3b302bc25c0d59e4
arnab.shil <ruddra90@gmail.com>
2023-03-03 17:31:16 +0100

    First commit

---
+++ aggregator-service/src/main/java/com/grpcflix/aggregator/controller/AggregatorController.java
@@ -0,0 +1,30 @@
+package com.grpcflix.aggregator.controller;
+
+import com.grpcflix.aggregator.dto.RecommendedMovie;
+import com.grpcflix.aggregator.dto.UserGenre;
+import com.grpcflix.aggregator.service.UserMovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class AggregatorController {
+
+    @Autowired
+    private UserMovieService userMovieService;
+
+    @GetMapping("/user/{loginId}")
+    public List<RecommendedMovie> getMovies(@PathVariable String loginId){
+        return this.userMovieService.getUserMovieSuggestions(loginId);
+    }
+
+    @PutMapping("/users")
+    public ResponseEntity<Object> setUserGenre(@RequestBody UserGenre userGenre){
+        this.userMovieService.setUserGenreObject(userGenre);
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
+
+}
